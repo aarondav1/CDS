5e9006f1bef27b3ada29a48152eacad18a8c87ea
aarondav1 <aaron-alvare@hotmail.com>
2022-08-10 11:36:33 -0500

    commit inicial

---
+++ src/main/java/com/mycompany/cds/CDS.java
@@ -0,0 +1,18 @@
+package com.mycompany.cds;
+/**
+ *
+ * @author aaron
+ * @version 1.0
+ * @since Clase principal del ejemplo.
+ * <p>
+ * <img src="https://www.udual.org/principal/wp-content/uploads/2021/09/UGlogo-1024x1024-1160x665.png" alt="logo"/>
+ */
+public class CDS {
+    /**
+     * Constructor de la clase principal CDS
+     * @param args 
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
